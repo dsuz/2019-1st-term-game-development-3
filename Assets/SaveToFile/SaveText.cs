120a958af203da1c682429fe0b8e14300479794c
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-04-26 00:21:53 +0900

    テキストをファイルに保存、ファイルからテキストを読み込むところを作った

---
+++ Assets/SaveToFile/SaveText.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;    // ファイルを操作するために必要
+
+public class SaveText : MonoBehaviour
+{
+    [SerializeField] string m_string = "あいうえおかきくけこさしすせそたちつてと";
+    /// <summary>データを保存するファイルのパス</summary>
+    string m_path;
+
+    private void Start()
+    {
+        m_path = Application.dataPath + "/text.txt";
+    }
+
+    /// <summary>テキストをファイルに保存する</summary>
+    public void Save()
+    {
+        StreamWriter writer = new StreamWriter(m_path);
+        writer.Write(m_string);
+        writer.Flush();
+        writer.Close();
+    }
+
+    /// <summary>ファイルからテキストを読み込む</summary>
+    public void Load()
+    {
+        string text = "";
+        StreamReader reader = new StreamReader(m_path);
+        text = reader.ReadToEnd();
+        reader.Close();
+        Debug.Log(text);
+    }
+}
