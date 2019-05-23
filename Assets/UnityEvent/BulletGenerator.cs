6fff172b1bfc3af8bff5ed527b55a1fc04c0ccc0
Daisuke Suzuki <bboydaisuke@gmail.com>
2019-05-24 01:18:22 +0900

    説明用ファイルを更新した

--- Assets/UnityEvent/BulletGenerator.cs
+++ Assets/UnityEvent/BulletGenerator.cs
@@ -18,6 +18,7 @@ public class BulletGenerator : MonoBehaviour
         go.transform.position = new Vector3(Random.Range(-3f, 3f), 0f, 0f);
 
         // イベントを登録する
-        GameObject.FindObjectOfType<WallController>().m_onCrash.AddListener(go.GetComponent<BulletController>().Fire);
+        //GameObject.FindObjectOfType<WallController>().m_onCrash.AddListener(go.GetComponent<BulletController>().Fire);    // パラメータなしの場合は普通に UnityEvent.AddListener を呼べばよい
+        GameObject.FindObjectOfType<WallController>().m_onCrashWithParams.AddListener(go.GetComponent<BulletController>().Fire);    // パラメータありの場合は抽象クラスを継承したクラスを作る必要があり多少面倒になる。WallController を参照せよ
     }
 }
--- Assets/UnityEvent/WallController.cs
+++ Assets/UnityEvent/WallController.cs
@@ -10,7 +10,8 @@ using UnityEngine.Events;
 public class WallController : MonoBehaviour
 {
     /// <summary>Wall に Ball がぶつかった時に呼ばれるイベント</summary>
-    public UnityEvent m_onCrash;
+    public UnityEvent m_onCrash = new UnityEvent();
+    public OnCrashWithParams m_onCrashWithParams = new OnCrashWithParams();
    
     private void OnCollisionEnter(Collision collision)
     {
@@ -18,7 +19,16 @@ public class WallController : MonoBehaviour
         if (collision.gameObject.tag == "Player")
         {
             m_onCrash.Invoke();
+            m_onCrashWithParams.Invoke(Vector3.up + Vector3.right, 3f); // パラメータ付きのイベントはこう呼び出す
         }
     }
+}
+
+/// <summary>
+/// パラメータ付きのイベントを呼び出すためのクラス
+/// </summary>
+[System.Serializable]
+public class OnCrashWithParams : UnityEvent<Vector3, float>
+{
 }
 
